845700a1b78b26ec8f086b13892737b39d8683d6
ankit23-exe <23it034@jssaten.ac.in>
2025-10-18 20:22:20 +0530

    service + portfolio done

--- backend/modals/featured_project_modal.js
+++ backend/modals/featured_project_modal.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 const featuredProjectSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  image: { type: String }, 
+  image: { type: String },
   tags: [{ type: String }],
-  techStack: [{ type: String }],
+  tech: [{ type: String }],
+  stats: [{
+    label: { type: String },
+    value: { type: String }
+  }],
   isFeatured: { type: Boolean},
-  
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('FeaturedProject', featuredProjectSchema);
