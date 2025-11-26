a3a0bd1400d5f78ffede0da1936c887078e2aa83
ankit23-exe <23it034@jssaten.ac.in>
2025-11-26 15:18:08 +0530

    hardcoded link done

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -60,7 +60,7 @@ export default function Navigation() {
   const portfolioUrl =
     (typeof import.meta !== "undefined" && import.meta.env && import.meta.env.VITE_PORTFOLIO_URL) ||
     (typeof process !== "undefined" && process.env && process.env.VITE_PORTFOLIO_URL) ||
-    "/";
+    "http://webstitchnextjs.s3-website.ap-south-1.amazonaws.com";
 
   return (
     <nav
